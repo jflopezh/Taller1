09446d8c7148455382a7302f8bb7bcbd4cf3354e
Estudiante <Estudiante@10.203.1.8>
2017-02-07 15:22:01 -0500

    Suma de Vectores

---
+++ src/taller/pkg1/Punto1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller.pkg1;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Punto1 {
+    public static void main(String[] args) {
+        java.util.Scanner Lectura = new java.util.Scanner(System.in);
+        float Largo = Lectura.nextFloat();
+        float Ancho = Lectura.nextFloat();
+        
+        System.out.print(Largo+" "+Ancho);
+        
+        
+        
+        
+    }
+}
